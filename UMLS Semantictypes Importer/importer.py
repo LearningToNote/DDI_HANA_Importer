0fc3689aed9dc3cabec15f75c32f13c12a0a9e30
Maximilian Grundke <maximiliangrundke@googlemail.com>
2016-03-03 18:25:51 +0100

    Add groups as special types

--- UMLS Semantictypes Importer/importer.py
+++ UMLS Semantictypes Importer/importer.py
@@ -12,13 +12,21 @@ connection = pyhdb.connect(
 )
 
 types = list()
-
 with open("UMLS Semantic Types with Codes and Groups.txt", "r") as f:
     for line in f:
         t = line.strip('\n, ').split("|")
         types.append((t[2], t[0], t[1], t[3]))
 
-connection.cursor().executemany("INSERT INTO LEARNING_TO_NOTE.TYPES(CODE, GROUP_ID, \"GROUP\", NAME) VALUES (?, ?, ?, ?)", types)
-
+cursor = connection.cursor()
+cursor.executemany("INSERT INTO LTN_DEVELOP.TYPES(CODE, GROUP_ID, \"GROUP\", NAME) VALUES (?, ?, ?, ?)", types)
 connection.commit()
+
+groups = list()
+cursor.execute('SELECT DISTINCT "GROUP_ID", "GROUP" FROM LTN_DEVELOP.TYPES')
+for row in cursor.fetchall():
+    groups.append((row[0], row[0], row[1], row[1]))
+
+cursor.executemany("INSERT INTO LTN_DEVELOP.TYPES(CODE, GROUP_ID, \"GROUP\", NAME) VALUES (?, ?, ?, ?)", groups)
+connection.commit()
+
 connection.close()
